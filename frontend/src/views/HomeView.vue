2a01a16ec78801d337642261ae5c97bcbd0cc670
Itsssammara <hoosenammara@gmail.com>
2024-03-08 15:58:35 +0200

    added navbar

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>home page</h1>
+    <img src="https://i.postimg.cc/J4KV8F7g/hoosenhijab3women.png" alt="">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    navigation,
+
+  }
+}
+</script>
